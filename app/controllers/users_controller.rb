5d98833747e2356b50a0ac3efee2a2ad0a305f35
keji-m <ken2mor@gmail.com>
2021-01-31 14:48:09 +0000

    users new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  get 'mypage', to: 'users#show'
+  get 'mypage/edit', to: 'users#edit'
+  resources :users, only: [:create, :update, :destroy]
 
 end
