da7de557281b83fbd570db74163a7fab788abeca
keji-m <ken2mor@gmail.com>
2021-02-01 14:46:17 +0000

    user signup login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :edit, :editoutline, :update, :destroy]
+  
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -8,14 +9,32 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーサーを登録しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザー登録に失敗しました'
+      render :new
+    end
   end
 
   def edit
+  end
+  
+  def editoutline
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
 
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   get 'mypage', to: 'users#show'
   get 'mypage/edit', to: 'users#edit'
+  get 'mypage/outline/edit', to: 'users#editoutline'
   resources :users, only: [:create, :update, :destroy]
 
 end
